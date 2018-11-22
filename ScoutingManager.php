14147621589b82d9cd9c578cad32abf4bd9a2599
mluo20 <mluo20@pascack.org>
2018-11-22 11:14:06 -0500

    add files

---
+++ ScoutingManager.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * This is the class for the main scouting manager, which should include everything that is necessary to be able to 
+ * manage the scouting server
+ */
+class ScoutingManager {
+
+	/*VARIABLES START HERE*/
+	
+	function __construct(argument) {
+		# code...
+	}
+
+}
